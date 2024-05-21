fb5653d230910b40bdcbd15d8311ad39e1913f1b
Isaac Datlof <ike18t@gmail.com>
2024-05-21 11:35:30 -0700

    chore: eslint w type info

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,14 @@ import tseslint from "typescript-eslint";
 
 export default [
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  { ignores: ['jest.config.ts', 'eslint.config.mjs'] },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
 ];
